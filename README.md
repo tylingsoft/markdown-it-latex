15780daf86769866ad05b91a6db8f08d3c21cacb
Tyler Long <tyler4long@gmail.com>
2017-03-26 17:39:31 +0800

    First fully working version

--- README.md
+++ README.md
@@ -19,12 +19,12 @@ const mdi = markdownIt()
 mdi.use(markdownItLatex)
 mdi.render('`$E = mc^2$`')
 mdi.render('`@(1/2[1-(1/2)^n])/(1-(1/2))=s_n@`')
-mdi.render('```math' +
-    '\oint_C x^3\, dx + 4y^2\, dy' +
-'```')
-mdi.render('```AsciiMath' +
-    'oint_Cx^3 dx+4y^2 dy' +
-'```')
+mdi.render(`\`\`\`math
+\oint_C x^3\, dx + 4y^2\, dy
+\`\`\``)
+mdi.render(`\`\`\`AsciiMath
+oint_Cx^3 dx+4y^2 dy
+\`\`\``)
 ```
 
 
--- test/index.js
+++ test/index.js
@@ -9,7 +9,19 @@ assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>', '# Hello w
 assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world')
 
 console.log(mdi.render('`$E = mc^2$`'))
+console.log(mdi.render(`\`\`\`math
+E = mc^2
+\`\`\``))
 console.log(mdi.render('`@2=(((3-x)xx2)/(3-x))@`'))
+console.log(mdi.render(`\`\`\`ascii-math
+2=(((3-x)xx2)/(3-x))
+\`\`\``))
 
 assert(mdi.render('`$E = mc^2$`').indexOf('katex-mathml') > -1)
+assert(mdi.render(`\`\`\`math
+E = mc^2
+\`\`\``).indexOf('katex-mathml') > -1)
 assert(mdi.render('`@2=(((3-x)xx2)/(3-x))@`').indexOf('katex-mathml') > -1)
+assert(mdi.render(`\`\`\`ascii-math
+2=(((3-x)xx2)/(3-x))
+\`\`\``).indexOf('katex-mathml') > -1)
